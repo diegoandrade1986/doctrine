f6186864d75f7a1e123f3e7a04e6e94f54fa03f6
diego <dieguinho_loko@hotmail.com>
2016-03-24 14:48:21 -0300

    Criado o Crud com o doctrine

--- src/Andrade/Sistema/Service/ClienteService.php
+++ src/Andrade/Sistema/Service/ClienteService.php
@@ -7,47 +7,77 @@
  */
 
 namespace Andrade\Sistema\Service;
-use Andrade\Sistema\Entity\Cliente;
-use Andrade\Sistema\Mapper\ClienteMapper;
+use Andrade\Sistema\Entity\Cliente as ClienteEntity;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
 
 class ClienteService
 {
-    private $cliente;
-    private $clienteMapper;
-    public function __construct(Cliente $cliente,ClienteMapper $clienteMapper)
+    private $em;
+    public function __construct(EntityManager $em)
     {
-        $this->cliente = $cliente;
-        $this->clienteMapper = $clienteMapper;
+        $this->em = $em;
     }
 
     public function insert(array $data)
     {
-        $clienteEntity = $this->cliente;
+        $clienteEntity = new ClienteEntity();
         $clienteEntity->setNome($data['nome']);
         $clienteEntity->setEmail($data['email']);
-
-        $mapper = $this->clienteMapper;
-        $result = $mapper->insert($clienteEntity);
-
-        return $result;
+        // persist -> inserindo no banco de dados passando um objeto
+        $this->em->persist($clienteEntity);
+        $this->em->flush();
+        return $clienteEntity;
     }
     public function update($id,array $array)
     {
-        return $this->clienteMapper->update($id,$array);
+        /*
+         Modo errado pois teria que realizar uma consulta novamente
+         /$repository = $this->em->getRepository("Andrade\Sistema\Entity\Cliente");
+        /**
+         * Dentro do repository conseguimos trabalhar com varios metodos
+
+        $cliente = $repository->find($id); //com isso ja temos a entidade cliente
+        */
+
+        $cliente = $this->em->getReference("Andrade\Sistema\Entity\Cliente",$id);
+        // getReference ela Imita como se tivesse buscado o cliente no banco
+        // ela eh uma entidade vazia mas para o nosso sistema ela e uma entidade correta para fazermos as nossas alteraçoes
+        $cliente->setNome($array['nome']);
+        $cliente->setEmail($array['email']);
+
+        $this->em->persist($cliente);
+        $this->em->flush();
+        return $cliente;
     }
 
     public function fetchAll()
     {
-        return $this->clienteMapper->fetchAll();
+        $repo = $this->em->getRepository("Andrade\Sistema\Entity\Cliente");
+        //$result =  $repo->getClientesOrdenados();
+        $result =  $repo->findAll();
+        return $result;
+
+        /*
+         * Documentaçao
+         * http://doctrine-orm.readt    hedocs.org/en/latest/reference/dql-doctrine-query-language.html#array-hydration
+        */
+
     }
 
     public function find($id)
     {
-        return $this->clienteMapper->find($id);
+        $repo = $this->em->getRepository("Andrade\Sistema\Entity\Cliente");
+        return $repo->find($id);
     }
     public function delete($id)
     {
-        return $this->clienteMapper->delete($id);
+        $cliente = $this->em->getReference("Andrade\Sistema\Entity\Cliente",$id);
+        // getReference ela Imita como se tivesse buscado o cliente no banco
+        // ela eh uma entidade vazia mas para o nosso sistema ela e uma entidade correta para fazermos as nossas alteraçoes
+        $this->em->remove($cliente);
+        return true;
+
     }
 
 }
